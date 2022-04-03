fb49599edc0df7a0c569a6e563f6c6436d86d8bb
wctestbank <wc.businessmail@gmail.com>
2022-04-02 21:01:26 -0400

    initial prompts

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+// fight function
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
