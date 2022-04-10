ae774407aa26691e8e6bff34d634f0df310add5f
wctestbank <wc.businessmail@gmail.com>
2022-04-09 20:18:56 -0400

    object optimization

--- assets/js/game.js
+++ assets/js/game.js
@@ -129,29 +129,11 @@ var shop = function () {
     switch (shopOptionPrompt) {
         case "REFILL": // new case
         case "refill":
-            if (playerInfo.money >= 7) {
-                window.alert("Refilling player's health by 20 for 7 dollars.");
-
-                playerInfo.health = playerInfo.health + 20;
-                playerInfo.money = playerInfo.money - 7;
-            }
-            else {
-                window.alert("You don't have enough money!");
-            }
-
+            playerInfo.refillHealth();
             break;
         case "UPGRADE": // new case
         case "upgrade":
-            if (playerInfo.money >= 7) {
-                window.alert("Upgrading player's attack by 6 for 7 dollars.");
-
-                playerInfo.attack = playerInfo.attack + 6;
-                playerInfo.money = playerInfo.money - 7;
-            }
-            else {
-                window.alert("You don't have enough money!");
-            }
-
+            playerInfo.upgradeAttack();
             break;
         case "LEAVE": // new case
         case "leave":
